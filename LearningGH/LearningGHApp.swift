26543f4693d9d5cb2cf9c0d30743faa733e2ccda
dhobbsthenoob <dhobbsthenoob@gmail.com>
2021-12-30 11:18:37 +0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by EDY on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
